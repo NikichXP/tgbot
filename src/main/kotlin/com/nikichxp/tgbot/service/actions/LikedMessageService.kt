0699ea15655a22f8503099f4064a56907622475b
Nikita <nikichx2@gmail.com>
2022-02-04 01:18:13 +0100

    debug text

--- src/main/kotlin/com/nikichxp/tgbot/service/actions/LikedMessageService.kt
+++ src/main/kotlin/com/nikichxp/tgbot/service/actions/LikedMessageService.kt
@@ -1,6 +1,8 @@
 package com.nikichxp.tgbot.service.actions
 
+import com.nikichxp.tgbot.core.CurrentUpdateProvider
 import com.nikichxp.tgbot.entity.MessageInteractionResult
+import com.nikichxp.tgbot.service.TgOperations
 import com.nikichxp.tgbot.service.UserService
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Service
@@ -8,7 +10,9 @@ import org.springframework.stereotype.Service
 @Service
 class LikedMessageService(
     private val mongoTemplate: MongoTemplate,
-    private val userService: UserService
+    private val userService: UserService,
+    private val currentUpdateProvider: CurrentUpdateProvider,
+    private val tgOperations: TgOperations
 ) {
 
     // TODO save history of karma givers
@@ -17,6 +21,8 @@ class LikedMessageService(
         userService.modifyUser(target) {
             it.rating += interaction.power
         }
+        val text = "${target.id} karma is set with participants ${interaction.users.toString()}"
+        tgOperations.sendMessage(currentUpdateProvider.update?.message?.chat?.id?.toString()!!, text)
      }
 
     // TODO this should be a part of some properties file with errors
