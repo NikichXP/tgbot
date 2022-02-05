9060ff7d3ce81e521be8129f36d5e98ad4da332b
Nikita <nikichx2@gmail.com>
2022-02-05 22:43:13 +0100

    fixed output of karma changes

--- src/main/kotlin/com/nikichxp/tgbot/service/actions/LikedMessageService.kt
+++ src/main/kotlin/com/nikichxp/tgbot/service/actions/LikedMessageService.kt
@@ -1,11 +1,14 @@
 package com.nikichxp.tgbot.service.actions
 
 import com.nikichxp.tgbot.core.CurrentUpdateProvider
+import com.nikichxp.tgbot.dto.User
 import com.nikichxp.tgbot.entity.MessageInteractionResult
 import com.nikichxp.tgbot.service.TgOperations
+import com.nikichxp.tgbot.service.UserInfo
 import com.nikichxp.tgbot.service.UserService
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicReference
 
 @Service
 class LikedMessageService(
@@ -17,17 +20,35 @@ class LikedMessageService(
 
     // TODO save history of karma givers
     fun changeRating(interaction: MessageInteractionResult) {
+        val actor = interaction.getActor()
+        val actorInfo = userService.getUserInfo(actor)
         val target = interaction.getTarget() ?: throw IllegalStateException(impossibleStateOfNoTarget)
+        val diff = calculateKarmaDiff(actorInfo, target, interaction)
+        val result = AtomicReference(0.0)
         userService.modifyUser(target) {
-            it.rating += interaction.power
+            it.rating += diff
+            result.set(it.rating)
         }
-        val text = "${target.id} karma is set with participants ${interaction.users.toString()}"
+        val text = "${getUserPrintName(actor)} changed karma of ${getUserPrintName(target)} (${result.get()})"
         tgOperations.sendMessage(currentUpdateProvider.update?.message?.chat?.id?.toString()!!, text)
-     }
+    }
+
+    private fun calculateKarmaDiff(actor: UserInfo, target: User, interaction: MessageInteractionResult): Double {
+        return 1 + (actor.rating / 10)
+    }
+
+    fun getUserPrintName(user: User): String {
+        return when {
+            user.username != null -> user.username
+            user.lastName != null -> "${user.firstName} ${user.lastName}"
+            else -> user.firstName
+        }
+    }
 
     // TODO this should be a part of some properties file with errors
     companion object {
-        const val impossibleStateOfNoTarget = "[INT0001: no interaction target found when it is supposed to be a target]"
+        const val impossibleStateOfNoTarget =
+            "[INT0001: no interaction target found when it is supposed to be a target]"
     }
 
 }
