2fdcadb240ce3785c3640e77f8c3e2c27a1d9431
Nikita <nikichx2@gmail.com>
2022-02-09 23:05:54 +0100

    index is responsible for deleting old docs

--- src/main/kotlin/com/nikichxp/tgbot/entity/UnparsedMessage.kt
+++ src/main/kotlin/com/nikichxp/tgbot/entity/UnparsedMessage.kt
@@ -1,12 +1,15 @@
 package com.nikichxp.tgbot.entity
 
 import org.bson.Document
+import org.springframework.data.mongodb.core.index.Indexed
+import java.time.LocalDateTime
 import java.util.*
 
 @org.springframework.data.mongodb.core.mapping.Document("unparsedMessage")
 data class UnparsedMessage(
         var content: Document,
-        var created: Long = System.currentTimeMillis()
+        @Indexed(name = "time_limited_index", expireAfter = "7d")
+        var created: LocalDateTime = LocalDateTime.now()
 ) {
 
     var id = UUID.randomUUID().toString()
