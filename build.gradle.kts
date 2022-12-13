46b00e9be71e05b2b27cc440764c1c52909fa539
Nikita <nikichx2@gmail.com>
2022-12-13 23:54:41 +0100

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,12 +27,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.7.6")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.6")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.21")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.21")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.ktor:ktor-client-core-jvm:2.1.3")
-    implementation("io.ktor:ktor-client-cio-jvm:2.1.3")
-    implementation("org.jetbrains.kotlin:kotlin-test:1.7.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio-jvm:$ktorVersion")
+    implementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.6")
 }
 
