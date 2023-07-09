c6199d7a029550b2c1e29e537a8b815f8c4fe7ee
Nikita Ruzhevsky <nikichx2@gmail.com>
2023-07-09 19:17:19 +0200

    echo controller method

--- src/main/kotlin/com/nikichxp/tgbot/api/InputController.kt
+++ src/main/kotlin/com/nikichxp/tgbot/api/InputController.kt
@@ -3,6 +3,8 @@ package com.nikichxp.tgbot.api
 import com.nikichxp.tgbot.entity.TgBot
 import com.nikichxp.tgbot.service.MessageEntryPoint
 import org.bson.Document
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,6 +16,9 @@ class InputController(
 ) {
 
     private val botMap = TgBot.values().associateBy { it.botName }
+
+    @GetMapping("/echo")
+    fun echo() = ResponseEntity.ok("ok")
 
     @PostMapping("/handle/{bot}")
     fun handle(@PathVariable bot: String, @RequestBody body: Document): String {
