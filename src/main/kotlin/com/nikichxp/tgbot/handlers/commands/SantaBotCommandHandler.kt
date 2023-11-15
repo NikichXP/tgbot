80bb21c5e256b9dc6e092432f92bf858d7161ac4
Nikita Ruzhevsky <nikichx2@gmail.com>
2023-11-15 17:08:25 +0100

    Update SantaBotCommandHandler.kt

--- src/main/kotlin/com/nikichxp/tgbot/handlers/commands/SantaBotCommandHandler.kt
+++ src/main/kotlin/com/nikichxp/tgbot/handlers/commands/SantaBotCommandHandler.kt
@@ -3,6 +3,7 @@ package com.nikichxp.tgbot.handlers.commands
 import com.nikichxp.tgbot.dto.Update
 import com.nikichxp.tgbot.entity.TgBot
 import com.nikichxp.tgbot.service.menu.CommandHandler
+import com.nikichxp.tgbot.service.tgapi.TgOperations
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.findById
 import org.springframework.stereotype.Service
@@ -12,7 +13,8 @@ import java.util.UUID
 // todo fuck clean code when you're out of time; whoever see this: remind me to finally refactor this shit
 @Service
 class SantaBotCommandHandler(
-    private val mongoTemplate: MongoTemplate
+    private val mongoTemplate: MongoTemplate,
+    private val tgOperations: TgOperations
 ) : CommandHandler {
 
     override fun supportedBots(tgBot: TgBot) = setOf(TgBot.SANTABOT)
@@ -26,23 +28,57 @@ class SantaBotCommandHandler(
                 val player = getSantaUserPlayerFromUpdate(update)
                 game.players += player
                 mongoTemplate.save(game)
+                tgOperations.replyToCurrentMessage("game created", update)
             }
+
             "/register" -> {
                 val gameId = args.first()
-                val game = mongoTemplate.findById<SecretSantaGame>(gameId) ?: throw IllegalArgumentException("game not found")
+                val game = getGame(gameId, update)
                 val player = getSantaUserPlayerFromUpdate(update)
+                var status = false
                 if (game.players.none { it.id == player.id }) {
                     game.players += player
+                    status = true
                 }
                 mongoTemplate.save(game)
+                tgOperations.replyToCurrentMessage(
+                    if (status) "Вы зарегистрировались в игре" else "Вы уже зарегистрированы в игре",
+                    update
+                )
             }
+
             "/ignore" -> {
                 val gameId = args[0]
-                val ignoreId = args[1]
-                // TODO have no time for this
+                val ignored = args[1]
+                val game = getGame(gameId, update)
+                val playerId = getSantaUserPlayerFromUpdate(update).id
+                val player = game.players.find { it.id == playerId } ?: throw IllegalArgumentException("register in game first")
+                player.ignores += ignored
+                mongoTemplate.save(game)
+            }
+
+            "/start" -> {
+                val gameId = args.first()
+                val game = getGame(gameId, update)
+                startGame(game, update)
             }
         }
         return true
+    }
+
+    private fun getGame(gameId: String, update: Update): SecretSantaGame {
+        return mongoTemplate.findById<SecretSantaGame>(gameId) ?: throw IllegalArgumentException("game not found")
+            .also { tgOperations.replyToCurrentMessage("Игра не найдена", update) }
+    }
+
+    private fun startGame(game: SecretSantaGame, update: Update) {
+        val users = game.players
+        val targets = game.players.map { it.username }
+
+        fun checkResult(): Boolean {
+
+        }
+
     }
 
     private fun getSantaUserPlayerFromUpdate(update: Update): SecretSantaPlayer {
@@ -62,5 +98,5 @@ data class SecretSantaPlayer(
     var id: Long,
     var username: String
 ) {
-    var ignores = listOf<String>()
+    var ignores = setOf<String>()
 }
