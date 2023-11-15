f25a7b35bba5bea7bafd621b5b76f5d6363ec6a8
Nikita <nikichx2@gmail.com>
2023-11-15 17:56:24 +0100

    Update SantaBotCommandHandler.kt

--- src/main/kotlin/com/nikichxp/tgbot/handlers/commands/SantaBotCommandHandler.kt
+++ src/main/kotlin/com/nikichxp/tgbot/handlers/commands/SantaBotCommandHandler.kt
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Service
 import java.lang.IllegalArgumentException
 import java.util.UUID
 
-// todo fuck clean code when you're out of time; whoever see this: remind me to finally refactor this shit
+// todo out of time - whoever see this: remind me to finally refactor this
 @Service
 class SantaBotCommandHandler(
-    private val mongoTemplate: MongoTemplate,
-    private val tgOperations: TgOperations
+        private val mongoTemplate: MongoTemplate,
+        private val tgOperations: TgOperations
 ) : CommandHandler {
 
     override fun supportedBots(tgBot: TgBot) = setOf(TgBot.SANTABOT)
@@ -42,8 +42,8 @@ class SantaBotCommandHandler(
                 }
                 mongoTemplate.save(game)
                 tgOperations.replyToCurrentMessage(
-                    if (status) "Вы зарегистрировались в игре" else "Вы уже зарегистрированы в игре",
-                    update
+                        if (status) "Вы зарегистрировались в игре" else "Вы уже зарегистрированы в игре",
+                        update
                 )
             }
 
@@ -52,7 +52,8 @@ class SantaBotCommandHandler(
                 val ignored = args[1]
                 val game = getGame(gameId, update)
                 val playerId = getSantaUserPlayerFromUpdate(update).id
-                val player = game.players.find { it.id == playerId } ?: throw IllegalArgumentException("register in game first")
+                val player = game.players.find { it.id == playerId }
+                        ?: throw IllegalArgumentException("register in game first")
                 player.ignores += ignored
                 mongoTemplate.save(game)
             }
@@ -68,23 +69,44 @@ class SantaBotCommandHandler(
 
     private fun getGame(gameId: String, update: Update): SecretSantaGame {
         return mongoTemplate.findById<SecretSantaGame>(gameId) ?: throw IllegalArgumentException("game not found")
-            .also { tgOperations.replyToCurrentMessage("Игра не найдена", update) }
+                .also { tgOperations.replyToCurrentMessage("Игра не найдена", update) }
     }
 
     private fun startGame(game: SecretSantaGame, update: Update) {
         val users = game.players
-        val targets = game.players.map { it.username }
+        val targets = game.players.map { it.username }.toMutableList()
 
         fun checkResult(): Boolean {
+            return users.zip(targets).all { (user, target) ->
+                user.ignores.none { it == target } && user.username != target
+            }
+        }
 
+        val rand = java.util.Random()
+        val size = users.size
+
+        while (!checkResult()) {
+            val a = rand.nextInt(size)
+            val b = rand.nextInt(size)
+            val tmp = targets[a]
+            targets[a] = targets[b]
+            targets[b] = tmp
+        }
+
+        users.zip(targets).forEach { (user, target) ->
+            tgOperations.sendMessage(
+                    user.id,
+                    "Ваша цель: @$target",
+                    update
+            )
         }
 
     }
 
     private fun getSantaUserPlayerFromUpdate(update: Update): SecretSantaPlayer {
         return SecretSantaPlayer(
-            id = update.message?.from?.id ?: throw IllegalArgumentException("not expected"),
-            username = update.message.from.username ?: throw IllegalArgumentException("you must have a username")
+                id = update.message?.from?.id ?: throw IllegalArgumentException("not expected"),
+                username = update.message.from.username ?: throw IllegalArgumentException("you must have a username")
         )
     }
 }
@@ -95,8 +117,8 @@ class SecretSantaGame {
 }
 
 data class SecretSantaPlayer(
-    var id: Long,
-    var username: String
+        var id: Long,
+        var username: String
 ) {
     var ignores = setOf<String>()
 }
