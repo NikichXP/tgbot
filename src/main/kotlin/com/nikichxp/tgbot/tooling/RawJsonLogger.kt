fd223356d3f50a3b5e8f7c0e235e5c4991c95a15
Nikita <nikichx2@gmail.com>
2024-03-23 01:54:58 +0100

    disable coroutine

--- src/main/kotlin/com/nikichxp/tgbot/tooling/RawJsonLogger.kt
+++ src/main/kotlin/com/nikichxp/tgbot/tooling/RawJsonLogger.kt
@@ -35,14 +35,14 @@ class RawJsonLogger(
     suspend fun logEvent(data: Document) = coroutineScope {
         if (storingEnabled) {
             val trace = EventTrace(data)
-            launch {
+//            launch {
                 try {
                     logger.info("start saving trace, id = ${trace.id}")
                     mongoTemplate.insert(trace)
                 } catch (e: Exception) {
                     logger.error("Failed to save trace", e)
                 }
-            }
+//            }
         }
     }
 }
