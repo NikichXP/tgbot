a2dd7c4f3c2ec1aa78cb3b6b360b4859f8387324
Nikita Ruzhevsky <nikichx2@gmail.com>
2024-09-29 03:12:40 +0200

    more cleanup

--- src/main/kotlin/com/nikichxp/tgbot/core/handlers/commands/CommandHandler.kt
+++ src/main/kotlin/com/nikichxp/tgbot/core/handlers/commands/CommandHandler.kt
@@ -2,7 +2,6 @@ package com.nikichxp.tgbot.core.handlers.commands
 
 import com.nikichxp.tgbot.core.dto.Update
 import com.nikichxp.tgbot.core.entity.TgBot
-import com.nikichxp.tgbot.core.entity.UpdateContext
 import org.springframework.stereotype.Indexed
 
 @Indexed
@@ -11,6 +10,4 @@ interface CommandHandler {
     fun isCommandForBotSupported(tgBot: TgBot): Boolean = supportedBots(tgBot).contains(tgBot)
     fun isCommandSupported(command: String): Boolean
     suspend fun processCommand(args: List<String>, command: String, update: Update): Boolean
-    suspend fun processCommand(args: List<String>, command: String, updateContext: UpdateContext): Boolean =
-        processCommand(args, command, updateContext.update)
 }
