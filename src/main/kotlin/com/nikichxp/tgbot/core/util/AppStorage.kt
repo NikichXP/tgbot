4cdfc5f73df9d5a4032f468e306851fc9dc0c40b
Nikita Ruzhevsky <nikichx2@gmail.com>
2024-10-07 02:45:02 +0200

    Create AppStorage.kt

---
+++ src/main/kotlin/com/nikichxp/tgbot/core/util/AppStorage.kt
@@ -0,0 +1,25 @@
+package com.nikichxp.tgbot.core.util
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.findById
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.stereotype.Service
+
+@Service
+class AppStorage(
+    private val mongoTemplate: MongoTemplate
+) {
+
+    fun saveData(data: AppData) = mongoTemplate.save(data)
+    fun saveData(key: String, value: String) = saveData(AppData(key, value))
+
+    fun getData(key: String) = mongoTemplate.findById<AppData>(key)
+
+}
+
+@Document("app_data")
+data class AppData(
+    @Id val key: String,
+    val value: String
+)
