42ea6e93ee9de885e8063bf22d6be462dedd68fc
Nikita <nikichx2@gmail.com>
2024-12-18 01:15:12 +0100

    new chat type field

--- src/main/kotlin/com/nikichxp/tgbot/core/dto/InlineQuery.kt
+++ src/main/kotlin/com/nikichxp/tgbot/core/dto/InlineQuery.kt
@@ -1,9 +1,13 @@
 package com.nikichxp.tgbot.core.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class InlineQuery(
     val id: String,
     val from: User,
     val location: Location? = null,
     val query: String,
-    val offset: String
+    val offset: String,
+    @JsonProperty("chat_type")
+    val chatType: String
 )
