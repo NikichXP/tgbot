59b6791ad922ddd6920d4f8852f2661c2174c793
nikichxp <nikichx2@gmail.com>
2025-02-09 23:15:58 +0100

    more work in progress

--- src/main/kotlin/com/nikichxp/tgbot/childcarebot/state/StateTransitionHandler.kt
+++ src/main/kotlin/com/nikichxp/tgbot/childcarebot/state/StateTransitionHandler.kt
@@ -1,38 +1,41 @@
 package com.nikichxp.tgbot.childcarebot.state
 
 import com.nikichxp.tgbot.childcarebot.ChildActivity
-import org.springframework.stereotype.Component
+import com.nikichxp.tgbot.childcarebot.TgMessageInfo
+import com.nikichxp.tgbot.core.dto.MessageId
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Indexed
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Indexed
 interface StateTransitionHandler {
 
-    fun from(state: ChildActivity): Boolean
-    fun to(state: ChildActivity): Boolean
+    fun from(): Set<ChildActivity>
+    fun to(): Set<ChildActivity>
 
-    fun doTransition(from: ChildActivity, to: ChildActivity)
+    fun doTransition(from: ChildActivity, to: ChildActivity, childId: Long)
 
 }
 
 @Service
-class StateTransitionHandlerFactory {
+class AddTimeNavigationHandler(
+    private val mongoTemplate: MongoTemplate,
+) : StateTransitionHandler {
 
-    // ???
+    override fun from() = setOf(ChildActivity.WAKE_UP)
+
+    override fun to() = setOf(ChildActivity.SLEEP)
+
+    override fun doTransition(from: ChildActivity, to: ChildActivity, childId: Long) {
+
+        TODO("Not yet implemented")
+    }
 
 }
 
-class AddTimeNavigationHandler : StateTransitionHandler {
-    override fun from(state: ChildActivity): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun to(state: ChildActivity): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun doTransition(from: ChildActivity, to: ChildActivity) {
-        TODO("Not yet implemented")
-    }
-
+class CallbackListen(var messageInfo: TgMessageInfo) {
+    lateinit var id: ObjectId
+    var callbackQuery = UUID.randomUUID().toString()
 }
