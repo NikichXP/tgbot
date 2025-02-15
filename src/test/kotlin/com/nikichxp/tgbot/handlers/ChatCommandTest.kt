474598d19727eda5d46a2592f98bb30ebd8f4ac1
nikichxp <nikichx2@gmail.com>
2025-02-15 09:23:51 +0100

    cleanup

--- src/test/kotlin/com/nikichxp/tgbot/handlers/ChatCommandTest.kt
+++ src/test/kotlin/com/nikichxp/tgbot/handlers/ChatCommandTest.kt
@@ -2,28 +2,18 @@ package com.nikichxp.tgbot.handlers
 
 import com.nikichxp.tgbot.core.dto.Update
 import com.nikichxp.tgbot.core.entity.TgBot
-import com.nikichxp.tgbot.core.handlers.BotSupportFeature
 import com.nikichxp.tgbot.core.handlers.ChatCommandsHandler
-import com.nikichxp.tgbot.core.handlers.commands.CommandHandler
-import com.nikichxp.tgbot.core.handlers.commands.CommandHandlerExecutor
-import com.nikichxp.tgbot.core.handlers.commands.CommandHandlerScanner
-import com.nikichxp.tgbot.core.handlers.commands.HandleCommand
-import com.nikichxp.tgbot.core.handlers.commands.SingleCommandHandler
+import com.nikichxp.tgbot.core.handlers.commands.*
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import org.mockito.Mockito
 import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.mongodb.core.query.update
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import java.util.concurrent.atomic.AtomicReference
-import kotlin.math.abs
-import kotlin.reflect.KFunction
 
 @SpringBootTest(classes = [ChatCommandsHandler::class, CommandHandlerExecutor::class])
 class ChatCommandTest {
@@ -33,9 +23,6 @@ class ChatCommandTest {
 
     @Autowired
     lateinit var chatCommandsHandler: ChatCommandsHandler
-
-    @Autowired
-    lateinit var commandHandlerExecutor: CommandHandlerExecutor
 
 
     companion object {
