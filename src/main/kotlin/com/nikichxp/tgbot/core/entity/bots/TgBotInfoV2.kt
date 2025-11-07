2ba49a24103188993008bd51486a5e4877d1895a
nikichxp <nikichx2@gmail.com>
2025-11-08 00:51:26 +0100

    remove unused code

--- src/main/kotlin/com/nikichxp/tgbot/core/entity/bots/TgBotInfoV2.kt
+++ src/main/kotlin/com/nikichxp/tgbot/core/entity/bots/TgBotInfoV2.kt
@@ -4,8 +4,4 @@ data class TgBotInfoV2(var name: String, var supportedFeatures: Set<String>) {
 
     constructor(bot: TgBotInfoV2Entity) : this(bot.name, bot.supportedFeatures)
 
-    fun getCorrespondingLegacyBot(): TgBot {
-        return TgBot.valueOf(name)
-    }
-
 }
