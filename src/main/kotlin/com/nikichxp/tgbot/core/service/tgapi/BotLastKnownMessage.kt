d7e23bb33b64536961c0a21d6635500ac6301d72
nikichxp <nikichx2@gmail.com>
2025-11-17 00:59:43 +0100

    fix date overflow

--- src/main/kotlin/com/nikichxp/tgbot/core/service/tgapi/BotLastKnownMessage.kt
+++ src/main/kotlin/com/nikichxp/tgbot/core/service/tgapi/BotLastKnownMessage.kt
@@ -1,5 +1,10 @@
 package com.nikichxp.tgbot.core.service.tgapi
 
 import java.time.LocalDateTime
+import java.time.Month
 
-data class BotLastKnownMessage(var id: String, var updateId: Long, var date: LocalDateTime = LocalDateTime.now())
+data class BotLastKnownMessage(var id: String, var updateId: Long, var date: LocalDateTime = MIN_DATE) {
+    companion object {
+        private val MIN_DATE = LocalDateTime.of(2017, Month.JANUARY, 1, 0, 0)
+    }
+}
